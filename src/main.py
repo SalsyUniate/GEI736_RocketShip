08ab1dfd4a62ea210be807b881b8b839d8fb18fa
Audric DAVID <audric.david@usherbrooke.ca>
2023-11-30 15:52:55 -0500

    c mieu + maj sim + maj gui

--- src/main.py
+++ src/main.py
@@ -15,10 +15,12 @@ import simulation as sim
 pxPerM = 100 # px/m
 
 def init_fuzzy():
+    THROTLE_MAX = 150
+
     x_rules_gauche = [
-        [3, 3, 2, 2, 1],
-        [2, 2, 1, 1, 1],
-        [2, 1, 0, 0, 0],
+        [3, 3, 2, 1, 1],
+        [2, 1, 1, 1, 1],
+        [1, 1, 0, 0, 0],
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0]
     ]
@@ -26,9 +28,9 @@ def init_fuzzy():
     x_rules_droit = [
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 2],
+        [0, 0, 0, 1, 1],
         [1, 1, 1, 2, 2],
-        [1, 2, 2, 3, 3]
+        [1, 1, 2, 3, 3]
     ]
 
     theta_rules_gauche = [
@@ -48,28 +50,28 @@ def init_fuzzy():
 	]
 
     y_rules_gauche = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 2, 3],
-        [0, 0, 0, 2, 3],
-        [0, 1, 2, 2, 3],
-        [1, 2, 3, 3, 3]
+        [2, 2, 2, 2, 3],
+        [2, 2, 2, 2, 3],
+        [2, 2, 2, 2, 3],
+        [2, 2, 2, 2, 3],
+        [2, 3, 3, 3, 3]
     ]
 
     y_rules_droit = y_rules_gauche
 
     rule_sets = {
         
-        'err_x': fz.SetTrig.autoN(5, -5, 5, True, 'err_x'),
-		'derr_x': fz.SetTrig.autoN(5, -5, 5, True, 'derr_x'),
+        'err_x': fz.SetTrig.autoN(5, -7.5, 7.5, True, 'err_x'),
+		'derr_x': fz.SetTrig.autoN(5, -15, 15, True, 'derr_x'),
 
         'err_theta': fz.SetTrig.autoN(5, -np.pi/2, np.pi/2, True, 'err_theta'),
 		'derr_theta': fz.SetTrig.autoN(5, -np.pi, np.pi, True, 'derr_theta'),
 
-        'err_y': fz.SetTrig.autoN(5, -5, 5, True, 'err_y'),
+        'err_y': fz.SetTrig.autoN(5, -1, 1, True, 'err_y'),
 		'derr_y': fz.SetTrig.autoN(5, -10, 10, True, 'derr_y'),
 
-        'force_gauche': fz.SetTrig.autoN(4, 0, 240, False, 'force_gauche'),
-		'force_droite': fz.SetTrig.autoN(4, 0, 240, False, 'force_droite')
+        'force_gauche': fz.SetTrig.autoN(4, 0, THROTLE_MAX, False, 'force_gauche'),
+		'force_droite': fz.SetTrig.autoN(4, 0, THROTLE_MAX, False, 'force_droite')
     }
 
     #X
@@ -96,10 +98,10 @@ def init_fuzzy():
 
 
     x_theta_rules_gauche = [
-        [0, 0, 1, 1],
-        [0, 1, 1, 2],
-        [1, 1, 2, 3],
-        [1, 2, 3, 3]
+        [0, 1, 2, 3],
+        [0, 1, 2, 3],
+        [1, 2, 2, 3],
+        [2, 3, 3, 3]
     ]
 
     """
@@ -113,15 +115,15 @@ def init_fuzzy():
     x_theta_rules_droit = x_theta_rules_gauche
 
     rule_sets_x_theta = {
-        'x_gauche': fz.SetTrig.autoN(4, 0, 3, True, 'x_gauche'),
-		'theta_gauche': fz.SetTrig.autoN(4, 0, 3, True, 'theta_gauche'),
+        'x_gauche': fz.SetTrig.autoN(4, 0, THROTLE_MAX, True, 'x_gauche'),
+		'theta_gauche': fz.SetTrig.autoN(4, 0, THROTLE_MAX, True, 'theta_gauche'),
         
-        'x_droit': fz.SetTrig.autoN(4, 0, 3, True, 'x_droit'),
-		'theta_droit': fz.SetTrig.autoN(4, 0, 3, True, 'theta_droit'),
+        'x_droit': fz.SetTrig.autoN(4, 0, THROTLE_MAX, True, 'x_droit'),
+		'theta_droit': fz.SetTrig.autoN(4, 0, THROTLE_MAX, True, 'theta_droit'),
 
         
-        'force_gauche': fz.SetTrig.autoN(4, 0, 240, False, 'force_gauche'),
-		'force_droite': fz.SetTrig.autoN(4, 0, 240, False, 'force_droite')
+        'force_gauche': fz.SetTrig.autoN(4, 0, THROTLE_MAX, False, 'force_gauche'),
+		'force_droite': fz.SetTrig.autoN(4, 0, THROTLE_MAX, False, 'force_droite')
     }
 
     
@@ -133,24 +135,24 @@ def init_fuzzy():
 
 
     xtheta_y_rules_gauche = [
-        [0, 2, 2, 3],
-        [1, 2, 3, 4],
-        [2, 3, 4, 5],
-        [3, 4, 5, 6]
+        [1, 2, 2, 3],
+        [2, 2, 3, 4],
+        [3, 4, 4, 5],
+        [4, 4, 5, 6]
     ]
 
     xtheta_y_rules_droit = xtheta_y_rules_gauche
 
     rule_sets_xtheta_y = {
-        'xtheta_gauche': fz.SetTrig.autoN(4, 0, 3, True, 'xtheta_gauche'),
-		'y_gauche': fz.SetTrig.autoN(4, 0, 3, True, 'y_gauche'),
+        'xtheta_gauche': fz.SetTrig.autoN(4, 0, THROTLE_MAX, True, 'xtheta_gauche'),
+		'y_gauche': fz.SetTrig.autoN(4, 0, THROTLE_MAX, True, 'y_gauche'),
         
-        'xtheta_droit': fz.SetTrig.autoN(4, 0, 3, True, 'xtheta_droit'),
-		'y_droit': fz.SetTrig.autoN(4, 0, 3, True, 'y_droit'),
+        'xtheta_droit': fz.SetTrig.autoN(4, 0, THROTLE_MAX, True, 'xtheta_droit'),
+		'y_droit': fz.SetTrig.autoN(4, 0, THROTLE_MAX, True, 'y_droit'),
 
         
-        'force_gauche': fz.SetTrig.autoN(7, 0, 240, False, 'force_gauche'),
-		'force_droite': fz.SetTrig.autoN(7, 0, 240, False, 'force_droite')
+        'force_gauche': fz.SetTrig.autoN(7, 0, THROTLE_MAX, False, 'force_gauche'),
+		'force_droite': fz.SetTrig.autoN(7, 0, THROTLE_MAX, False, 'force_droite')
     }
 
     f_rules_gauche = [ fz.Rule(fz.AND(rule_sets_xtheta_y['xtheta_gauche'][xtheta_i],rule_sets_xtheta_y['y_gauche'][y_i]), rule_sets_xtheta_y['force_gauche'][xtheta_y_rules_gauche[xtheta_i][y_i]]) for xtheta_i in range(4) for y_i in range(4) ]
@@ -173,148 +175,150 @@ def init_fuzzy():
     }
 
 def main():
+	pygame.init()
+	windowSize = (1300,700)
+	simSize = (20,20)
+	screen = pygame.display.set_mode(windowSize)
+	pygame.display.set_caption("Rocket control")
+	clock = pygame.time.Clock()
+	fps = 60
+	
 	space = pymunk.Space()
 	space.gravity = (0.0, 9.81)
-	
-	rocket = sim.Rocket(space, 0.5, 1).set_pos(Vec2d(2,0.1))
-	target = sim.Target(Vec2d(2,-5))
+
+	rocket = sim.Rocket(space, 0.5, 1).set_pos((15,15))
+	target = sim.Target(Vec2d(10,10))
 	objects = [
-		sim.Ground(space, [(-1000,1),(1000,1)]),
+		sim.Ground(space, [(0,0),(0,simSize[1]), (simSize[0],simSize[1]), (simSize[0],0),(0,0)]),
 		rocket,
 		target
-	]
+	]	
+
+	leftSelected = True
+	boosterStrength = [100,100]
 	
-	mode = 'normal'
-	if mode == 'training':
+	theta = 0
+	dest_x = 10
+	dest_y = 10
+	dx = 0
+	dy = 0
+	dt = 0.016
+	
+	fz_sys = init_fuzzy()
+	fuzzy = False
+	
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT : sys.exit(0)
+			
+
+		rocketpos = rocket.get_pos()
+		old_theta = theta
+		old_dx = dx
+		old_dy = dy
 		
-		rocketposRangeX = (0, 8) ; rocketposRangeY = (0, 3)
-		targetRangeX = (0, 8) ; targetRangeY = (0, 3)
+		dx = -(dest_x - rocketpos.x)
+		dy = -(dest_y - rocketpos.y)
 		
-		def fitness(sys):
-			target.pos = Vec2d(random.uniform(*targetRangeX), random.uniform(*targetRangeY))
-			rocket.set_pos(Vec2d(random.uniform(*rocketposRangeX), random.uniform(*rocketposRangeY)))
+		theta = rocket.get_angle() % (2*np.pi)
+		if(theta > np.pi): theta = theta - 2*np.pi
+		theta = -theta
+		
+		d_dx = (dx - old_dx) / (dt)
+		d_dy = (dy - old_dy) / (dt)
+		d_theta = (theta - old_theta) / (dt)
+		
+		x_gauche = fz_sys['x_gauche'].compute({'err_x': dx, 'derr_x': d_dx}, tag = 'force_gauche')
+		x_droit = fz_sys['x_droit'].compute({'err_x': dx, 'derr_x': d_dx}, tag = 'force_droite')
+		
+		theta_gauche = fz_sys['theta_gauche'].compute({'err_theta': theta, 'derr_theta': d_theta}, tag = 'force_gauche')
+		theta_droit = fz_sys['theta_droit'].compute({'err_theta': theta, 'derr_theta': d_theta}, tag = 'force_droite')
+		
+		y_gauche = fz_sys['y_gauche'].compute({'err_y': dy, 'derr_y': d_dy}, tag = 'force_gauche')
+		y_droit = fz_sys['y_droit'].compute({'err_y': dy, 'derr_y': d_dy}, tag = 'force_droite')
+		
+		xtheta_gauche = fz_sys['xtheta_gauche'].compute({'x_gauche': x_gauche, 'theta_gauche': theta_gauche}, tag = 'force_gauche')
+		xtheta_droit = fz_sys['xtheta_droit'].compute({'x_droit': x_droit, 'theta_droit': theta_droit}, tag = 'force_droite')
+		
+		force_gauche = fz_sys['gauche'].compute({'xtheta_gauche': xtheta_gauche, 'y_gauche': y_gauche}, tag = 'force_gauche')
+		force_droite = fz_sys['droit'].compute({'xtheta_droit': xtheta_droit, 'y_droit': y_droit}, tag = 'force_droite')
+		
+		if(theta > np.pi / 2): force_droite = 0
+		if(theta < -np.pi / 2): force_gauche = 0
+
+		if fuzzy: rocket.apply_force(force_gauche, force_droite)
+		print(f'x : {rocketpos.x}, y : {rocketpos.y}, theta : {theta}, force_gauche : {force_gauche}, force_droite : {force_droite}')
+	
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_SPACE]: fuzzy = not(fuzzy)
+		if keys[pygame.K_ESCAPE] : sys.exit(0)
+		if keys[pygame.K_d]   : # booster gauche
+			rocket.apply_force(boosterStrength[0],0)
+			rocket.propsvisibility[0] = True
+		else:
+			rocket.propsvisibility[0] = False
+		if keys[pygame.K_q]  : # booster gauche
+			rocket.apply_force(0,boosterStrength[1])
+			rocket.propsvisibility[1] = True
+		else:
+			rocket.propsvisibility[1] = False
+		if keys[pygame.K_r]: # reset rocket
+			rocket.set_pos(Vec2d(5,5))
 			rocket.set_angle(0)
 			rocket.set_linvel(Vec2d(0,0))
-			rocket.set_rotvel(0)
-			
-			t = 0
-			while t <= 8:
-				# fuzzy control
-				err = target.pos - rocket.get_pos() ; vel = rocket.get_linvel()
-				f = sys.compute({
-					'errx': err.x, 'erry': err.y,
-					'velx': vel.x, 'vely': vel.y,
-					'angle': rocket.get_angle(), 'velangle': rocket.get_rotvel()
-				})
-				rocket.apply_force(f['fl'], f['fr'])
-				
-				space.step(1/20)
-				
-				t += dt
-			
-			return 1/(1+(err**2 + vel**2)) *20
+			rocket.set_rotvel(0) 
+		if keys[pygame.K_LEFT]:
+			leftSelected = True
+		if keys[pygame.K_RIGHT]:
+			leftSelected = False
+		if keys[pygame.K_UP]:
+			if leftSelected :
+				boosterStrength[0] += 5
+				if boosterStrength[0] > 100 :
+					boosterStrength[0] = 100
+			else :
+				boosterStrength[1] += 5
+				if boosterStrength[1] > 100 :
+					boosterStrength[1] = 100
+		if keys[pygame.K_DOWN]:
+			if leftSelected :
+				boosterStrength[0] -= 5
+				if boosterStrength[0] < 0 :
+					boosterStrength[0] = 0
+			else :
+				boosterStrength[1] -= 5
+				if boosterStrength[1] < 0 :
+					boosterStrength[1] = 0
 		
-		insets = [
-			fz.SetGauss(0,1, -5,5,       tag='errx'),
-			fz.SetGauss(0,1, -5,5,       tag='velx'),
-			fz.SetGauss(0,1, -10,5,      tag='erry'),
-			fz.SetGauss(0,1, -5,5,       tag='vely'),
-			fz.SetGauss(0,1, 0,2*pi,     tag='angle'),
-			fz.SetGauss(0,1, -8*pi,8*pi, tag='velangle')
-		]
-		outsets = [
-			fz.SetGauss(0,1, 0,10000, tag='fl'),
-			fz.SetGauss(0,1, 0,10000, tag='fr')
-		]
-		nbrules = 100
-		rules = [ fz.Rule(fz.Cond(fz.AND(*[dcp(fin) for fin in insets])), dcp(fout)) for i in range(nbrules) for fout in outsets]
+		screen.fill((255,255,255))
+	
+		rocket.apply_damping(2,2)
+		space.step(1/fps)
 		
-		pop = fz.Population(rules)
-		pop.init(1000)
-		pop.evolve(fitness, 100)
-		
-	else:
-		
-		pygame.init()
-		ws = (1000, 600)
-		screen = pygame.display.set_mode(ws)
-		pygame.display.set_caption("Rocket control")
-		clock = pygame.time.Clock()
-		keysdown = {}
-		fps = 60
-		
-		theta = 0
-		dest_x = 2
-		dest_y = -5
-		dx = 0
-		dy = 0
-		dt = 0.016
-		
-		fz_sys = init_fuzzy()
-		
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT : sys.exit(0)
-				
+		offset = (-int(rocket.body.position.x*pxPerM)+windowSize[0]/2,-int(rocket.body.position.y*pxPerM)+windowSize[1]/2)
 
-			rocketpos = rocket.get_pos()
-			old_theta = theta
-			old_dx = dx
-			old_dy = dy
-			
-			dx = -(dest_x - rocketpos.x)
-			dy = -(dest_y - rocketpos.y)
-			
-			theta = rocket.get_angle() % (2*np.pi)
-			if(theta > np.pi): theta = theta - 2*np.pi
-			theta = -theta
-			
-			d_dx = (dx - old_dx) / (dt*1000)
-			d_dy = (dy - old_dy) / (dt*1000)
-			d_theta = (theta - old_theta) / (dt*1000)
-			
-			x_gauche = fz_sys['x_gauche'].compute({'err_x': dx, 'derr_x': d_dx}, tag = 'force_gauche') // 80
-			x_droit = fz_sys['x_droit'].compute({'err_x': dx, 'derr_x': d_dx}, tag = 'force_droite') // 80
-			
-			theta_gauche = fz_sys['theta_gauche'].compute({'err_theta': theta, 'derr_theta': d_theta}, tag = 'force_gauche') // 80
-			theta_droit = fz_sys['theta_droit'].compute({'err_theta': theta, 'derr_theta': d_theta}, tag = 'force_droite') // 80
-			
-			y_gauche = fz_sys['y_gauche'].compute({'err_y': dy, 'derr_y': d_dy}, tag = 'force_gauche') // 80
-			y_droit = fz_sys['y_droit'].compute({'err_y': dy, 'derr_y': d_dy}, tag = 'force_droite') // 80
-			
-			xtheta_gauche = fz_sys['xtheta_gauche'].compute({'x_gauche': x_gauche, 'theta_gauche': theta_gauche}, tag = 'force_gauche') // 80
-			xtheta_droit = fz_sys['xtheta_droit'].compute({'x_droit': x_droit, 'theta_droit': theta_droit}, tag = 'force_droite') // 80
-			
-			force_gauche = fz_sys['gauche'].compute({'xtheta_gauche': xtheta_gauche, 'y_gauche': y_gauche}, tag = 'force_gauche')
-			force_droite = fz_sys['droit'].compute({'xtheta_droit': xtheta_droit, 'y_droit': y_droit}, tag = 'force_droite')
-			
-			if(theta > np.pi / 2): force_droite = 0
-			if(theta < -np.pi / 2): force_gauche = 0
-    
-			keys = pygame.key.get_pressed()
-			if keys[pygame.K_ESCAPE] : sys.exit(0)
-			"""
-			if keys[pygame.K_LEFT]   : rocket.apply_force(100,0)
-			if keys[pygame.K_RIGHT]  : rocket.apply_force(0,100)
-			"""
-			rocket.apply_force(force_gauche, force_droite)
-			print(f'x : {rocketpos.x}, y : {rocketpos.y}, theta : {theta}, force_gauche : {force_gauche}, force_droite : {force_droite}')
-			if keys[pygame.K_r]: # reset rocket
-				rocket.set_pos(Vec2d(2,0.1))
-				rocket.set_angle(0)
-				rocket.set_linvel(Vec2d(0,0))
-				rocket.set_rotvel(0) 
-			
-			screen.fill((255,255,255))
-		
-			space.step(1/fps)
-			
-			rocketpos = rocket.get_pos()
-			offset = (ws[0]/2 - int(rocketpos.x*pxPerM), ws[1]/2 - int(rocketpos.y*pxPerM))
-			for obj in objects : obj.draw(screen, offset)
-		
-			pygame.display.flip()
-			dt = clock.tick(fps) / 1000
+		for obj in objects : obj.draw(screen,offset)
+
+		if leftSelected:
+			pygame.draw.polygon(screen, "red", [(20,windowSize[1]/2-105),(20,windowSize[1]/2+105),(55,windowSize[1]/2+105),(55,windowSize[1]/2-105)])
+			pygame.draw.polygon(screen, "black", [(windowSize[0]-20,windowSize[1]/2-105),(windowSize[0]-20,windowSize[1]/2+105),(windowSize[0]-55,windowSize[1]/2+105),(windowSize[0]-55,windowSize[1]/2-105)])
+		else:
+			pygame.draw.polygon(screen, "black", [(20,windowSize[1]/2-105),(20,windowSize[1]/2+105),(55,windowSize[1]/2+105),(55,windowSize[1]/2-105)])
+			pygame.draw.polygon(screen, "red", [(windowSize[0]-20,windowSize[1]/2-105),(windowSize[0]-20,windowSize[1]/2+105),(windowSize[0]-55,windowSize[1]/2+105),(windowSize[0]-55,windowSize[1]/2-105)])
+
+		pygame.draw.polygon(screen, "cyan", [(25,windowSize[1]/2+100),(50,windowSize[1]/2+100),(50,windowSize[1]/2+(100-boosterStrength[0]*2)),(25,windowSize[1]/2+(100-boosterStrength[0]*2))])
+		pygame.draw.polygon(screen, "cyan", [(windowSize[0]-25,windowSize[1]/2+100),(windowSize[0]-50,windowSize[1]/2+100),(windowSize[0]-50,windowSize[1]/2+(100-boosterStrength[1]*2)),(windowSize[0]-25,windowSize[1]/2+(100-boosterStrength[1]*2))])
+
+		minimapSize = (200,200)
+		minimap = pygame.Surface(minimapSize)  # the size of your rect
+		minimap.set_alpha(128)                # alpha level
+		minimap.fill((128,128,128))           # this fills the entire surface
+		pygame.draw.circle(minimap, "red", (int((rocket.body.position.x/simSize[0])*minimapSize[0]), int((rocket.body.position.y/simSize[1])*minimapSize[1])), 5)
+		pygame.draw.circle(minimap, "green", (int((target.pos.x/simSize[0])*minimapSize[0]), int((target.pos.y/simSize[1])*minimapSize[1])), 5)
+		screen.blit(minimap, (10,windowSize[1]-(minimapSize[1]+10))) 
+
+		pygame.display.flip()
+		clock.tick(fps)
 
 if __name__ == '__main__':
     sys.exit(main())
--- src/simulation.py
+++ src/simulation.py
@@ -2,14 +2,16 @@ import pymunk
 from pymunk.vec2d import Vec2d
 import pygame
 import math
+import numpy as np
 
 
 pi = math.pi
 pxPerM = 100 # px/m
 
+
 class Shape:
-	def set_pos(self, pos):
-		self.shape.body.position = pos
+	def set_pos(self, x, y):
+		self.shape.body.position = Vec2d(x,y)
 		self.shape.space.reindex_shapes_for_body(self.shape.body)
 		return self
 class CircleShape(Shape):
@@ -21,9 +23,9 @@ class CircleShape(Shape):
 		
 		space.add(self.shape)
 	
-	def draw(self, screen,offset):
+	def draw(self, screen,offset,color="blue"):
 		pos = self.shape.offset.rotated(self.shape.body.angle) + self.shape.body.position
-		pygame.draw.circle(screen, "blue", (int(pxPerM*pos.x)+offset[0],int(pxPerM*pos.y)+offset[1]), int(pxPerM*self.shape.radius))
+		pygame.draw.circle(screen, color, (int(pxPerM*pos.x)+offset[0], int(pxPerM*pos.y)+offset[1]), int(pxPerM*self.shape.radius))
 class PolyShape(Shape):
 	def __init__(self, space,body, pnts, m):
 		self.shape = pymunk.Poly(body, pnts)
@@ -33,12 +35,12 @@ class PolyShape(Shape):
 		
 		space.add(self.shape)
 	
-	def draw(self, screen,offset):
+	def draw(self, screen, offset,color = "blue"):
 		pnts = []
 		for v in self.shape.get_vertices():
 			pos = v.rotated(self.shape.body.angle) + self.shape.body.position
 			pnts.append((int(pxPerM*pos.x)+offset[0],int(pxPerM*pos.y)+offset[1]))
-		pygame.draw.polygon(screen, "blue", pnts)
+		pygame.draw.polygon(screen, color, pnts)
 class RectShape(PolyShape):
 	def __init__(self, space,body, w,h, m):
 		pnts = [(-w/2,-h/2),(-w/2,h/2),(w/2,h/2),(w/2,-h/2)]
@@ -51,7 +53,7 @@ def shapeBody(ShapeClass, space, *args, **kwargs):
 	return shape
 
 class Ground:
-	def __init__(self, space, pnts, thick=0.05):		
+	def __init__(self, space, pnts, thick=0.20):		
 		self.segments = [ pymunk.Segment(space.static_body, pnts[i], pnts[i-1], thick) for i in range(1,len(pnts)) ]
 		for seg in self.segments:
 			seg.elasticity = 0.7 
@@ -63,23 +65,25 @@ class Ground:
 		
 		space.add(*self.segments)
 	
-	def draw(self, screen,offset):
-		pygame.draw.lines(screen, "red", False, [(int(pxPerM*pnt[0])+offset[0],int(pxPerM*pnt[1])+offset[1]) for pnt in self.pnts], int(pxPerM*self.thick*2))
-		for pnt in self.pnts : pygame.draw.circle(screen, "red", (int(pxPerM*pnt[0])+offset[0],int(pxPerM*pnt[1])+offset[1]), int(pxPerM*self.thick))
+	def draw(self, screen, offset):
+		pygame.draw.lines(screen, "red", False, [(int(pxPerM*pnt[0] + offset[0]),int(pxPerM*pnt[1] + offset[1])) for pnt in self.pnts], int(pxPerM*self.thick*2))
+		for pnt in self.pnts : pygame.draw.circle(screen, "red", (int(pxPerM*pnt[0] + offset[0]),int(pxPerM*pnt[1] + offset[1])), int(pxPerM*self.thick))
 
 class Rocket:
 	def __init__(self, space, w,h):
 		self.body = pymunk.Body()
 		space.add(self.body)
 		
-		self.hull = RectShape(space,self.body, w,h, 10)
-		self.props = [CircleShape(space,self.body, w/4, 1), CircleShape(space,self.body, w/4, 1)]
+		self.hull = PolyShape(space,self.body, [(-0.25,0.),(-0.1875,-0.25),(0.,-0.5),(0.1875,-0.25),(0.25,0.),(0.25,0.5),(-0.25,0.5)], 10)
+		self.props = [PolyShape(space,self.body, [(-0.125,-0.0625),(-0.0625,-0.125),(0.0625,-0.125),(0.125,-0.0625),(0.0625,0.125),(-0.0625,0.125)], 1), PolyShape(space,self.body, [(-0.125,-0.0625),(-0.0625,-0.125),(0.0625,-0.125),(0.125,-0.0625),(0.0625,0.125),(-0.0625,0.125)], 1)]
 		
-		self.forcepos = [(-w/2, h/2),(w/2, h/2)]
-		self.forceangle = [pi/4, pi/4]
+		self.forcepos = [(-0.25, 0.5),(0.25, 0.5)]
+		self.forceangle = [11*pi/8, 13*pi/8]
 		
-		self.props[0].shape.unsafe_set_offset(self.forcepos[0])
-		self.props[1].shape.unsafe_set_offset(self.forcepos[1])
+		self.props[0].shape.unsafe_set_vertices(self.props[0].shape.get_vertices(),pymunk.Transform.rotation(self.forceangle[0]).translation(*self.forcepos[0]))
+		self.props[1].shape.unsafe_set_vertices(self.props[1].shape.get_vertices(),pymunk.Transform.rotation(self.forceangle[1]).translation(*self.forcepos[1]))
+  
+		self.propsvisibility = [False, False]
 	
 	def set_pos(self, pos):
 		self.body.position = pos
@@ -105,17 +109,27 @@ class Rocket:
 	def get_rotvel(self):
 		return self.body.angular_velocity
 	
+	def apply_damping(self, lin,rot):
+		self.body.apply_force_at_world_point(-self.body.velocity*lin, self.body.position)
+		# torque
+		rot = -self.body.angular_velocity*rot
+		self.body.apply_force_at_local_point(Vec2d(rot/2,0), Vec2d(0, -1))
+		self.body.apply_force_at_local_point(Vec2d(-rot/2,0), Vec2d(0, 1))
+		
 	def apply_force(self, l, r):
-		self.body.apply_force_at_local_point((0,-l), self.forcepos[0])
-		self.body.apply_force_at_local_point((0,-r), self.forcepos[1])
+		self.body.apply_force_at_local_point((np.cos(self.forceangle[0])*l,np.sin(self.forceangle[0])*l), self.forcepos[0])
+		self.body.apply_force_at_local_point((np.cos(self.forceangle[1])*r,np.sin(self.forceangle[1])*r), self.forcepos[1])
 		return self
 	
 	def draw(self, screen, offset):
-		self.hull.draw(screen, offset)
-		for prop in self.props : prop.draw(screen, offset)
-
+		self.hull.draw(screen,offset)
+		for i in range(len(self.props)) : 
+			if self.propsvisibility[i] :
+				self.props[i].draw(screen,offset,"red")
+			else:
+				self.props[i].draw(screen,offset)
 class Target:
 	def __init__(self, pos):
 		self.pos = pos
 	def draw(self, screen,offset):
-		pygame.draw.circle(screen, "red", (int(pxPerM*self.pos.x)+offset[0],int(pxPerM*self.pos.y)+offset[1]), 10)
+		pygame.draw.circle(screen, "green", (int(pxPerM*self.pos.x)+offset[0],int(pxPerM*self.pos.y)+offset[1]), 10)
