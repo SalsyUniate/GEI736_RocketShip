dab218b9aea600000f72b77aa70f5e6c9dfc94a9
TheoMaitreDuMondeConnuEtPlusEncore <theo-39@hotmail.fr>
2023-11-30 14:06:58 -0500

    angle done

--- src/physique.py
+++ src/physique.py
@@ -79,8 +79,10 @@ class Rocket:
 		self.forcepos = [(-0.25, 0.5),(0.25, 0.5)]
 		self.forceangle = [11*pi/8, 13*pi/8]
 		
-		self.props[0].shape.unsafe_set_vertices(self.props[0].shape.get_vertices(),pymunk.Transform.rotation(self.forceangle[0]).translation(*self.forcepos[0]))
-		self.props[1].shape.unsafe_set_vertices(self.props[1].shape.get_vertices(),pymunk.Transform.rotation(self.forceangle[1]).translation(*self.forcepos[1]))
+		self.props[0].shape.unsafe_set_vertices(self.props[0].shape.get_vertices(),pymunk.Transform.rotation(pi/2 + self.forceangle[1]))
+		self.props[0].shape.unsafe_set_vertices(self.props[0].shape.get_vertices(),pymunk.Transform.translation(*self.forcepos[0]))
+		self.props[1].shape.unsafe_set_vertices(self.props[1].shape.get_vertices(),pymunk.Transform.rotation(pi/2 + self.forceangle[0]))
+		self.props[1].shape.unsafe_set_vertices(self.props[1].shape.get_vertices(),pymunk.Transform.translation(*self.forcepos[1]))
   
 		self.propsvisibility = [False, False]
 	
